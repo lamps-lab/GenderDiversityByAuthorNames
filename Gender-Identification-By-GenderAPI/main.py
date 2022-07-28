f3739245b1144841e8aab1e9e8a0aaf2dfddffe6
yasasi <yaabeysinghe@gmail.com>
2022-07-28 18:48:45 -0400

    Added a script to read names from a csv file and write the names with their gender

---
+++ Gender-Identification-By-GenderAPI/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import NameToGender
+
+
+def read_data():
+    df = pd.read_csv("Data/names.csv", usecols=["first_name", "last_name"])
+    return df
+
+
+def get_genders_for_name(df):
+    new_df = pd.DataFrame({"first_name": [], "last_name": [], "api_gender": []})
+    for index, row in df.iterrows():
+        first_name = row["first_name"]
+        last_name = row["last_name"]
+        new_data = pd.DataFrame({
+            "first_name": [first_name],
+            "last_name": [last_name],
+            "api_gender": [NameToGender.get_gender(first_name, last_name)]
+        })
+        new_df = pd.concat([new_df, new_data])
+
+    new_df.to_csv("./Data/names_and_genders.csv")
+
+
+df = read_data()
+get_genders_for_name(df)
+

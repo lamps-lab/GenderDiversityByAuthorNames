61224ba3cc2b833f48b14bbe75eef1a432e1d867
yasasi <yaabeysinghe@gmail.com>
2022-08-19 19:49:14 -0400

    Updated the diversity indicator code

--- DiversityIndicator.py
+++ DiversityIndicator.py
@@ -3,30 +3,31 @@ from scipy.stats import entropy
 
 
 def read_data():
-    df = pd.read_csv("Data/all_gender_api.csv", usecols=["first_name", "last_name", "api_gender"])
+    df = pd.read_csv("data/output/author_names_and_genders.csv", usecols=["first_name", "last_name", "gender"])
     return df
 
 
 def get_number_of_male_female_count(df):
-    df = df.groupby(['api_gender'])["api_gender"].count()
+    df = df.groupby(['gender'])["gender"].count()
     female = df.get("female")
     male = df.get("male")
     unknown = df.get("unknown")
     return female, male, unknown
 
 
-def get_gender_diversity_indicator(female, male):
+def get_gender_diversity(female, male):
     total = female + male
     m = male/total  # proportion of male
     f = female/total  # proportion of female
     print("Proportion of Male: " + str(m))
     print("Proportion of Female: " + str(f))
     cross_entropy_metric = entropy([m, f], base=2)
-    print("Gender Diversity Indicator: " + str(cross_entropy_metric))
     return cross_entropy_metric
 
 
-df = read_data()
-female, male, unknown = get_number_of_male_female_count(df)
-# ignore unknown genders
-gender_diversity_indicator = get_gender_diversity_indicator(female, male)
+def get_diversity_indicator():
+    df = read_data()
+    female, male, unknown = get_number_of_male_female_count(df)
+    # ignore unknown genders
+    gender_diversity_indicator = get_gender_diversity(female, male)
+    return gender_diversity_indicator

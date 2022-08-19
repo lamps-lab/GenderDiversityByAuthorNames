05bf138c573268d022a73c397681b85675d19da7
yasasi <yaabeysinghe@gmail.com>
2022-08-19 17:05:13 -0400

    Added Error Handling to Gender API

--- GenderAPINameToGender.py
+++ GenderAPINameToGender.py
@@ -1,12 +1,17 @@
 import requests
 import GenderAPIConstants
 
-API_Key = Constants.API_Key
+API_Key = GenderAPIConstants.API_Key
 
 
 def get_gender(firstname, lastname):
     url = 'https://gender-api.com/get?split=' + firstname + '%20' + lastname + '&key=' + API_Key
     r = requests.get(url)
-    response = r.json()
-    gender = response["gender"]
-    return gender
+    if r.status_code == 200:
+        response = r.json()
+        gender = response["gender"]
+        return gender
+    else:
+        print("[ERROR] Error in gender-api.com GET API request")
+        print("[ERROR] Status Code " + str(r.status_code))
+        exit(0)
